fe5d5ee35e961ef9c58d3044be5bb69bee360fa5
Akshit Garg <garg.akshit@gmail.com>
2021-05-07 18:40:40 +0530

    fix: ssl
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- config/prod.exs
+++ config/prod.exs
@@ -45,7 +45,9 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :percentage_calc, PercentageCalcWeb.Endpoint, force_ssl: [hsts: true]
+config :percentage_calc, PercentageCalcWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
